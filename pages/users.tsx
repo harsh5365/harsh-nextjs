aab84f541f6d4f49bea66a94f00676b2cf1796e7
harsh bhatt <48681264+harsh5365@users.noreply.github.com>
2022-07-19 23:30:18 +0530

    users page

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,28 +1,44 @@
-import type { NextPage } from 'next'
+import type { NextComponentType, NextPage } from 'next'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const Users: NextPage = (props: any) => {
+const Users: NextPage = (props:any) => {
     return (
-        <div>
-            <h1>Users</h1>
-            <p>This is the users page</p>
-            <ul className="list-group">
-                {props.users.map(user=> (
-                    <li className="list-group-item" key={user.id}>
-                        {user.first_name}
-                    </li>
-                ))}
-            </ul>
+        <div className='container'>
+            <h1 className='text-center'>Users</h1>
+            <p className='text-center'>This is the users page</p>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <TableRow users={props.users} key={1} />
+                </tbody>
+            </table>
         </div>
+    )
+}
+
+const TableRow: any = (props: any) => {
+    return (
+        props.users.map((user: {id: number, first_name: string, last_name: string, email: string}) => {
+            console.log(user);
+            <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+            </tr>
+        })
     )
 }
 
 Users.getInitialProps = async () => {
     const user_data = await fetch('https://reqres.in/api/users');
     const users = await user_data.json();
-    return {
-        users: users.data
-    }
+    return { users: users.data }
 }
 
 
