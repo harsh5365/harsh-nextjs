f3b5c29a0f0ddb6c7b2d6076008b796572e45f3e
harsh bhatt <48681264+harsh5365@users.noreply.github.com>
2022-07-27 23:42:59 +0530

    added display, will find better way to paginate

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,37 +1,50 @@
 import type { NextPage } from 'next'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
+import { Card, Col, Pagination, Row } from 'react-bootstrap'
 
-const Users: NextPage = (props:any) => {
+type singleUser = {
+        id: number,
+        first_name: string,
+        last_name: string,
+        email: string,
+        avatar: string
+    };
+
+type Props = {
+    users: Array<singleUser>
+}
+
+const Users: NextPage = (props: Props) => {
     return (
         <div className='container'>
             <h1 className='text-center'>Users</h1>
             <p className='text-center'>This is the users page</p>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.users.map((user:any, i:number) => (
-                        <TableRow user={user} key={i} />
-                    ))}
-                </tbody>
-            </table>
+            <Row>
+                {
+                    props.users.map((user: singleUser, i: number) => (
+                        <Col key={i} md={3}>
+                            <Card key={i}>
+                                <Card.Body>
+                                    <Card.Img variant='top' src={user.avatar}></Card.Img>
+                                    <Card.Title>{user.first_name} {user.last_name}</Card.Title>
+                                    <Card.Text>
+                                        {user.email}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                    )
+                }
+            </Row>
+            <footer>
+                <Pagination>
+                    <Pagination.Prev />
+                    <Pagination.Next />
+                </Pagination>
+            </footer>
         </div>
-    )
-}
-
-const TableRow: React.ComponentType = (props: any) => {
-    return (
-        <tr key={props.user.id}>
-            <td>{props.user.first_name}</td>
-            <td>{props.user.last_name}</td>
-            <td>{props.user.email}</td>
-        </tr>
     )
 }
 
